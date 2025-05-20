165b82969f7656b9804569f60e0bf473e78d4e2a
sugrbear <avk639@utexas.edu>
2025-05-20 15:36:44 -0500

    Filter GET api data by gym query param

--- models/DataPoint.js
+++ models/DataPoint.js
@@ -1,8 +1,19 @@
 const mongoose = require('mongoose');
 
 const dataPointSchema = new mongoose.Schema({
-  timestamp: { type: Date, default: () => new Date() },
-  count: { type: Number, required: true },
+  gym: {
+    type: String,
+    required: true   // we now require the gym identifier
+  },
+  timestamp: {
+    type: Date,
+    default: () => new Date()
+  },
+  count: {
+    type: Number,
+    required: true
+  }
 });
 
 module.exports = mongoose.model('DataPoint', dataPointSchema);
+
