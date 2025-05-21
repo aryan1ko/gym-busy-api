b1a672bf86abd57e3b410c67551cfa855e76d192
sugrbear <avk639@utexas.edu>
2025-05-20 20:25:40 -0500

    Update DataPoint.js
    reverted before errors

--- models/DataPoint.js
+++ models/DataPoint.js
@@ -1,19 +1,8 @@
-// models/DataPoint.js
 const mongoose = require('mongoose');
 
 const dataPointSchema = new mongoose.Schema({
-  gym: {
-    type: String,
-    required: true                    // every point must have a gym
-  },
-  timestamp: {
-    type: Date,
-    default: () => new Date()
-  },
-  count: {
-    type: Number,
-    required: true
-  }
+  timestamp: { type: Date, default: () => new Date() },
+  count: { type: Number, required: true },
 });
 
 module.exports = mongoose.model('DataPoint', dataPointSchema);
